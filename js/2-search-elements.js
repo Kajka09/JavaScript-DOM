27c4baf05c9a4d70e2b948d465578460503acf84
Kajka09 <kajkastefaniak@gmail.com>
2017-02-05 11:45:58 +0100

    wyszukiwanie elementów

---
+++ js/2-search-elements.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var linki = document.getElementsByClassName('link');
+console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName ('a');
+console.log(linkiPoTagu);
+
+var linkiPoSelektorze = document.querySelectorAll ('.superlink');
+console.log(linkiPoSelektorze);
+
+var paragrafPoID = document.querySelector('#parSecond');
+console.log(paragrafPoID);
