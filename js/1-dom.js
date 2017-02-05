334f88b1e4b39842ac8caaf3f9a7cf1cdc5aea45
Kajka09 <kajkastefaniak@gmail.com>
2017-02-05 11:26:55 +0100

    dom

---
+++ js/1-dom.js
@@ -0,0 +1,10 @@
+'use strict';
+ 
+console.log(document.documentElement); 
+console.log(document.body);
+console.log(document.head);
+
+
+console.log(window);
+console.log(window.innerHeight);
+//logowanie obiektu elementu
